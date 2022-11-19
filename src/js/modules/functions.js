85046d07948bc8a7f8322547bade0e955d19e40f
voronine <84736877+voronine@users.noreply.github.com>
2022-11-19 19:57:07 +0200

    Add files via upload

---
+++ src/js/modules/functions.js
@@ -0,0 +1,15 @@
+// Поека поддержки webpб добаление класса webp или no webp
+export function isWebp() {
+    function testWebP(callback) {
+        let webP = new Image();
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2);
+        };
+        webP.src = "data:image/webp;base64,UklGRkAAAABXRUJQVlA4IDQAAADwAQCdASoBAAEAAQAcJaACdLoB+AAETAAA/vW4f/6aR40jxpHxcP/ugT90CfugT/3NoAAA"
+    }
+    //Добавление класса _webp или no_webp для HTML
+    testWebP(function (support) {
+        let className = support === true ? 'webp' : 'no-webp';
+        document.documentElement.classList.add(className);
+    })
+}
